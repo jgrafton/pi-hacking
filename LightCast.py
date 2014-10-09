6b23e78fc4daeb00957753b5b3017285370e8f9d
Fred Licht <flicht@apple.com>
2014-10-09 12:14:20 -0700

    More cleanup

--- LightCast.py
+++ LightCast.py
@@ -1,41 +1,58 @@
-import time
+#!/usr/bin/env python
+
+import os
+import sys
 import datetime
-import json
-from pprint import pprint
-from twython import Twython
-
-
-# Load our keys
-twitter_key_file=open('tweetkey.json')
-twitter_keys = json.load(twitter_key_file)
-
-# Twitter application authentication
-APP_KEY = twitter_keys['APP_KEY']
-APP_SECRET = twitter_keys['APP_SECRET']
-OAUTH_TOKEN = twitter_keys['OAUTH_TOKEN']
-OAUTH_TOKEN_SECRET = twitter_keys['OAUTH_TOKEN_SECRET']
-
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-# TODO:  Capture the light values from CLI.
-
-# Posting here for debug use only
-green_value = 1
-yellow_value = 0
-red_value = 0
-
-epoc_time = int(datetime.datetime.now().strftime("%s"))
-payload={"green":green_value,"yellow":yellow_value,"red":red_value,"timestamp":epoc_time }
-
-print json.dumps(payload)
-
-# Create tweet post
 try:
-    #twitter.verify_credentials()
-    twitter.update_status(status=json.dumps(payload))
-except KeyboardInterrupt:
-    pass
+    import json
+except:
+    import simplejson as json
+from twython import Twython
+import optparse
 
 
+def collect_command_args():
+    # Collect the command line arguments to determine work to be performed.
+    parser = optparse.OptionParser()
+    parser.add_option("-r", "--red", action="store", type="string", dest="red", help='The State of the red light, options are ON or OFF')
+    parser.add_option("-g", "--green", action="store", type="string", dest="green", help='The State of the green light, options are ON or OFF')
+    parser.add_option("-y", "--yellow",  action="store", type="string", dest="yellow", help='The State of the yellow light, options are ON or OFF')
 
+    if(len(sys.argv) < 2):
+        parser.print_help()
+        os._exit(0)
 
+    return parser
+
+# The actual RUN block for the whole script.
+if __name__ == '__main__':
+
+    # Load our keys
+    twitter_key_file = open('tweetkey.json')
+    twitter_keys = json.load(twitter_key_file)
+
+    # Twitter application authentication
+    APP_KEY = twitter_keys['APP_KEY']
+    APP_SECRET = twitter_keys['APP_SECRET']
+    OAUTH_TOKEN = twitter_keys['OAUTH_TOKEN']
+    OAUTH_TOKEN_SECRET = twitter_keys['OAUTH_TOKEN_SECRET']
+
+    #Create the twitter object with our authentication
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+    # Posting here for debug use only
+    green_value = 1
+    yellow_value = 0
+    red_value = 0
+
+    epoc_time = int(datetime.datetime.now().strftime("%s"))
+    payload = {"green": green_value, "yellow": yellow_value, "red": red_value, "timestamp": epoc_time}
+
+    print json.dumps(payload)
+
+    # Create tweet post
+    try:
+        #twitter.verify_credentials()
+        twitter.update_status(status=json.dumps(payload))
+    except KeyboardInterrupt:
+        pass
