90d2994904bfb9cad7cacbd241f1a9a72f225c8f
flicht <flicht@apple.com>
2014-10-08 23:35:32 -0700

    Added draft script to push updates to twitter Adding tweetkey.json.example and ignoring the actual tweetkey.json for safety of our keys

---
+++ LightCast.py
@@ -0,0 +1,46 @@
+import time
+import json
+from pprint import pprint
+import RPi.GPIO as GPIO
+from twython import TwythonStreamer
+
+
+# Set the GPIO Channel
+LED=11
+
+# Load our keys
+twitter_key_file=open('tweetkey.json')
+twitter_keys = json.load(twitter_key_file)
+
+# Twitter application authentication
+APP_KEY = twitter_keys['APP_KEY']
+APP_SECRET = twitter_keys['APP_SECRET']
+OAUTH_TOKEN = twitter_keys['OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = twitter_keys['OAUTH_TOKEN_SECRET']
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print data['text'].encode('utf-8')
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(1.0)
+            GPIO.output(LED, GPIO.LOW)
+        # Want to disconnect after the first result?
+        # self.disconnect()
+
+    def on_error(self, status_code, data):
+        print status_code, data
+
+# Setup GPIO as output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+# Create streamer
+try:
+    stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    stream.user()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+
