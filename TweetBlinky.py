50c1b50e20b06b4f486e19f83fad8cd3d7238853
Pi <john.grafton@gmail.com>
2014-10-08 19:18:50 -0700

    FIrst post of the twit'n twatt

---
+++ TweetBlinky.py
@@ -0,0 +1,37 @@
+import time
+import RPi.GPIO as GPIO
+from twython import TwythonStreamer
+
+# Search terms
+TERMS = '#yes'
+
+# GPIO pin number of LED
+LED = 11
+
+# Twitter application authentication
+APP_KEY = 'WzpVxGq7GkBbhuFOHnhMxVtDZ'
+APP_SECRET = 'emO0CQkB7sNAX4XSfMuf8DuLFMBp7viecLewy3OG8K2EMHmgY2'
+OAUTH_TOKEN = '648983-weByKvXtqd9UMVcdPI41GANrV91vIW3miOd4GHBUFku'
+OAUTH_TOKEN_SECRET = 'HbH3WEG4bsqo8qs0pcOboSCu6uHV0Jf8PibefaSHLmdoQ'
+
+# Setup callbacks from Twython Streamer
+class BlinkyStreamer(TwythonStreamer):
+        def on_success(self, data):
+                if 'text' in data:
+                        print data['text'].encode('utf-8')
+                        print
+                        GPIO.output(LED, GPIO.HIGH)
+                        time.sleep(0.5)
+                        GPIO.output(LED, GPIO.LOW)
+
+# Setup GPIO as output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+# Create streamer
+try:
+        stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        stream.statuses.filter(track=TERMS)
+except KeyboardInterrupt:
+        GPIO.cleanup()
