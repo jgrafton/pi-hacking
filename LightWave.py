8a06f4023cf277cb3922fd744c46271894a2ea8c
Pi <john.grafton@gmail.com>
2014-10-09 20:27:06 -0700

    Solved the light problem, added nicer initilization and start checks

--- LightWave.py
+++ LightWave.py
@@ -6,8 +6,39 @@ from twython import TwythonStreamer
 
 
 # GPIO pin number of LED
-LEDS = {"RED": 4, "YELLOW": 2, "GREEN": 3}
+LEDS = {'RED': 4, 'YELLOW': 2, 'GREEN': 3}
 
+# Setup each light GPIO as output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LEDS["RED"], GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LEDS["YELLOW"], GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LEDS["GREEN"], GPIO.OUT, initial=GPIO.LOW)
+
+
+def toggle_light(light, state):
+    if light in LEDS:
+        if int(state) == 1:
+            GPIO.output(LEDS[light], GPIO.HIGH)
+        elif int(state) == 0:
+            GPIO.output(LEDS[light], GPIO.LOW)
+        else:
+	    print "Debug: unknown state found: " + state
+
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            try:
+                payload = json.loads(data['text'].encode('utf-8'))
+                print payload
+                for key, value in payload.iteritems():
+		    print "Payload info: "+ key, value
+		    toggle_light(key, value)
+            except Exception:
+                print "ignoring tweet: " + payload
+
+    def on_error(self, status_code, data):
+        print status_code, data
 
 # Load our keys
 twitter_key_file = open('tweetkey.json')
@@ -20,46 +51,20 @@ OAUTH_TOKEN = twitter_keys['OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = twitter_keys['OAUTH_TOKEN_SECRET']
 
 
-def toggle_light(light, state):
-    if light in LEDS:
-	if state == 1:
-            GPIO.output(LEDS[light], GPIO.HIGH)
-        elif state == 0:
-	    GPIO.output(LEDS[light], GPIO.LOW)
-        else:
-	    pass
-
-
-class MyStreamer(TwythonStreamer):
-    def on_success(self, data):
-        if 'text' in data:
-            #try:
-                payload = json.loads(data['text'])
-        	print payload
-		for key,value in payload:
-                    toggle_light( key, value )
-            #except Exception:
-		#print "ignoring tweet: " + data['text']
-
-    def on_error(self, status_code, data):
-        print status_code, data
-
-# Setup each light GPIO as output
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LEDS["RED"], GPIO.OUT)
-GPIO.output(LEDS["RED"], GPIO.LOW)
-GPIO.setup(LEDS["YELLOW"], GPIO.OUT)
-GPIO.output(LEDS["YELLOW"], GPIO.LOW)
-GPIO.setup(LEDS["GREEN"], GPIO.OUT)
-GPIO.output(LEDS["GREEN"], GPIO.LOW)
-
 # Create streamer
 try:
-    toggle_light("RED", 1 )
-    time.sleep(1)
-    toggle_light("RED", 0 )
+    print "GPIO RPI Revision: ", GPIO.RPI_REVISION
+    print "GPIO Version: ", GPIO.VERSION
+    print "Beginning Initilization sequence. Please standby"
+    for key, value in LEDS.iteritems():
+        print "Initilizing light ", key
+        toggle_light(key, 1)
+        time.sleep(1)
+        toggle_light(key, 0)
+    print "Initilization Complete.  Waiting for incoming data:"
     stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     stream.user()
 except:
+    print "Exiting..."
     GPIO.cleanup()
 
